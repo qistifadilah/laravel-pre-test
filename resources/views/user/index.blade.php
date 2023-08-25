68e4728ccaaf5fac6e04ff238124833ba79f51a6
Qisti Fadilah <qistifadilah01@gmail.com>
2023-08-25 13:40:05 +0700

    first commit

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Media Online</h1>
+<h3>Social Media Developer</h3>
+<p>Belajar dan berbagi agar hidup menjadi lebih baik</p>
+<p>
+    <strong>Benefit Join di Media Online</strong>
+</p>
+<ul>
+  <li>Mendapatkan motivasi dari sesama Developer</li>
+  <li>Sharing knowladge</li>
+  <li>Dibuat oleh calon Developer terbaik</li>
+</ul> 
+<p>
+  <strong>Cara Bergabung ke Media Online</strong>
+</p>
+<ol>
+  <li>Mengunjungi Website ini</li>
+  <li>Mendaftar di <a href="/form">Form Sign Up</a></li>
+  <li>Selesai!</li>
+</body>
+</html>
+
